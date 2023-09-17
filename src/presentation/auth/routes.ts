d4acbd8bbb3215b051ed401ee6e6b502243830db
mavro <mrojasb2000@gmail.com>
2023-09-17 00:23:32 -0300

    chore: add testing repository access from routes

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { AuthDatasource, AuthRepository } from "../../domain";
+import { AuthDatasourceImpl, AuthRepositoryImpl } from "../../infrastructure";
 
 export class AuthRoutes {
     static get routes(): Router {
         const router = Router();
-        const controller = new AuthController();
+
+        // Datasource and Repository
+        const datasource: AuthDatasource = new AuthDatasourceImpl();
+        const authRepository: AuthRepository = new AuthRepositoryImpl(datasource);
+
+        const controller = new AuthController(authRepository);
 
         router.use('/login', controller.loginUser)
         router.use('/register', controller.registerUser)
